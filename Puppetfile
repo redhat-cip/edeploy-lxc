6770f7ad5552a780b83b1cc32c611a80c5e5ae56
Sebastien Badia <sebastien.badia@enovance.com>
2013-12-10 16:03:36 +0100

    Launch tests: simpler/easier

--- Puppetfile
+++
@@ -1,4 +0,0 @@
-forge "http://forge.puppetlabs.com"
-
-mod 'puppetlabs/apt', '1.4.0'
-mod 'thias/libvirt', '0.2.3'
--- puppet-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-sudo apt-get install ruby-dev build-essential -y
-sudo gem install --no-ri --no-rdoc -v 0.9.10 librarian-puppet
-sudo librarian-puppet install --verbose
-sudo puppet apply --certname edeploy-ci --modulepath $(pwd)/puppet/modules:$(pwd)/puppet/ext-modules edeploy-ci.pp
