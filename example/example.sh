67c4854d6d8d96e0473242862f8222b0f67d58e5
Gonéri Le Bouder <goneri.lebouder@enovance.com>
2013-12-11 13:48:30 +0100

    example: add Debian support

--- example/example.sh
+++ example/example.sh
@@ -9,7 +9,11 @@
 
 set -e
 
+PUPPETMASTER="os-ci-test4.lab"
+DEBIAN=1
+
 [ -d manifests ] || git clone gitolite@git.labs.enovance.com:openstack-puppet-ci.git -b master manifests
+
 cd manifests
 git pull
 cd ..
@@ -24,25 +28,37 @@ cd ..
 
 sudo ../edeploy-lxc --config config.yaml restart
 
-rsync -av manifests modules root@os-ci-test4.lab:/etc/puppet
-ssh root@os-ci-test4.lab service mysqld restart
-echo "create database puppet;" | ssh root@os-ci-test4.lab mysql -uroot
-echo "grant all privileges on puppet.* to puppet@localhost identified by 'password';" | ssh root@os-ci-test4.lab mysql -uroot
-ssh root@os-ci-test4.lab yum install -y ruby-mysql rubygem-activerecord
-# https://ask.puppetlabs.com/question/3853/error-400-on-server-failed-to-submit-replace-facts-command-connection-refused/
+if [ -z $DEBIAN ]; then
+    mysqld="mysqld"
+    ssh root@${PUPPETMASTER} yum install -y ruby-mysql rubygem-activerecord
+else
+    mysqld="mysql"
+    ssh root@${PUPPETMASTER} apt-get install --yes ruby-mysql ruby-activerecord
+fi
+
+rsync -av manifests modules root@${PUPPETMASTER}:/etc/puppet
+ssh root@${PUPPETMASTER} service $mysqld restart
+echo "create database puppet;" | ssh root@${PUPPETMASTER} mysql -uroot
+echo "grant all privileges on puppet.* to puppet@localhost identified by 'password';" | ssh root@${PUPPETMASTER} mysql -uroot
+
 
 sudo bash -c 'echo *.enovance.com > /var/lib/lxc/os-ci-test4/rootfs/etc/puppet/autosign.conf'
-ssh root@os-ci-test4.lab augtool set '/files/etc/puppet/puppet.conf/master/storeconfigs' 'true'
-ssh root@os-ci-test4.lab augtool set '/files/etc/puppet/puppet.conf/master/dbadapter' 'mysql'
-ssh root@os-ci-test4.lab augtool set '/files/etc/puppet/puppet.conf/master/dbuser' 'puppet'
-ssh root@os-ci-test4.lab augtool set '/files/etc/puppet/puppet.conf/master/dbpassword' 'password'
-ssh root@os-ci-test4.lab augtool set '/files/etc/puppet/puppet.conf/master/dbserver' 'localhost'
-ssh root@os-ci-test4.lab puppet master
+set +e
+ssh root@${PUPPETMASTER} augtool -s set '/files/etc/puppet/puppet.conf/master/storeconfigs' 'true'
+ssh root@${PUPPETMASTER} augtool -s set '/files/etc/puppet/puppet.conf/master/dbadapter' 'mysql'
+ssh root@${PUPPETMASTER} augtool -s set '/files/etc/puppet/puppet.conf/master/dbuser' 'puppet'
+ssh root@${PUPPETMASTER} augtool -s set '/files/etc/puppet/puppet.conf/master/dbpassword' 'password'
+ssh root@${PUPPETMASTER} augtool -s set '/files/etc/puppet/puppet.conf/master/dbserver' 'localhost'
+ssh root@${PUPPETMASTER} augtool -s set '/files/etc/puppet/puppet.conf/master/storeconfigs' 'true'
+set -e
+ssh root@${PUPPETMASTER} puppet master
 
 for i in `cat config.yaml|awk '/^ +address: 192.168.134./ {print $2}'`; do
-    ssh root@$i augtool set '/files/etc/puppet/puppet.conf/main/server' 'os-ci-test4.enovance.com'
-    ssh root@$i cp /bin/false /usr/bin/yum
-    ssh root@$i cp /bin/false /usr/bin/apt-get
+    set +e
+    ssh root@$i augtool -s set '/files/etc/puppet/puppet.conf/main/server' 'os-ci-test4.enovance.com'
+    set -e
+#    ssh root@$i cp /bin/false /usr/bin/yum
+#    ssh root@$i cp /bin/false /usr/bin/apt-get
     ssh root@$i service puppet stop
     ssh root@$i puppet agent --onetime --no-daemonize --debug
 done
