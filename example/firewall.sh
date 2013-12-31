83e8a5738dc93a7219ae950728f4abb2e1914dda
Gonéri Le Bouder <goneri.lebouder@enovance.com>
2013-12-31 15:23:13 +0100

    example: turns NAT on

---
+++ example/firewall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+dev=$(ip -4 r|awk '/^default/ {print $5}')
+iptables -A FORWARD -i bibi0 -j ACCEPT
+iptables -A FORWARD -o bibi0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o $dev -j MASQUERADE
+echo 1 > /proc/sys/net/ipv4/ip_forward
